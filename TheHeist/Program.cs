02f53fbaf9d2c72e6edf20ebcc8938c477e59796
Jordan Smith <jordansmith989@gmail.com>
2021-01-28 20:45:32 -0600

    the heist

---
+++ TheHeist/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("Type your new team member's name:");
+            var memberName = Console.ReadLine();
+            Console.WriteLine("Type your new team member's skill level from 0 - 10");
+            var memberSkill = Console.ReadLine();
+            var skillNumber = int.Parse(memberSkill);
+            Console.WriteLine("Type your new team member's courage from 0.0 - 5.0");
+            var memberCourage = Console.ReadLine();
+            var courageDecimal = double.Parse(memberCourage);
+            var teamMember = new TeamMember(memberName, skillNumber, courageDecimal);
+            teamMember.showTeamMember();
+        }
+    }
+}
---
+++ TheHeist/TeamMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHeist
+{
+    class TeamMember
+    {
+        public string Name { get; set;}
+        public int SkillLevel { get; set; }
+        public double Courage { get; set; }
+
+        public TeamMember(string name, int skillLevel, double courage)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            Courage = courage;
+        }
+
+        public void showTeamMember()
+        {
+            Console.WriteLine($"Name: {Name}, Skill Level: {SkillLevel} out of 10, Courage: {Courage} out of 5.0");
+        }
+    }
+}
