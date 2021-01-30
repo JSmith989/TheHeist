853bd15bd8b8786b38e59da74730c2b29e5dbe2d
Jordan Smith <jordansmith989@gmail.com>
2021-01-30 11:16:48 -0600

    heist complete

---
+++ TheHeist/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHeist
+{
+    class Bank
+    {
+        public int Difficulty { get; set; }
+        public int Luck { get; set; }
+
+        public Bank(int difficulty)
+        {
+            Difficulty = difficulty;
+        }
+
+        public void LadyLuck()
+        {
+            var random = new Random();
+            Luck = random.Next(-10, 10);
+        }
+    }
+}
--- TheHeist/Program.cs
+++ TheHeist/Program.cs
@@ -7,9 +7,14 @@ namespace TheHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("Plan Your Heist! Enter the bank difficulty number:");
             // create a list to store team members
             var teamMembers = new List<TeamMember> { };
+            int bankDifficulty = int.Parse(Console.ReadLine());
+            int skill = 0;
+            var newBank = new Bank(bankDifficulty);
+            int success = 0;
+            int fail = 0;
 
             bool input = true;
             do
@@ -21,7 +26,7 @@ namespace TheHeist
                     input = false;
                     continue;
                 }
-                Console.WriteLine("Type your new team member's skill level from 0 - 10");
+                Console.WriteLine("Type your new team member's skill level from 0 - 100");
                 var memberSkill = Console.ReadLine();
                 var skillNumber = int.Parse(memberSkill);
                 Console.WriteLine("Type your new team member's courage from 0.0 - 2.0");
@@ -36,11 +41,38 @@ namespace TheHeist
 
             // .Count shows number of team members
             Console.WriteLine($"Number of team members: {teamMembers.Count}");
+            Console.WriteLine("Please enter your trial runs:");
+
+            //takes number of trial runs
+            var trialRuns = int.Parse(Console.ReadLine());
 
             foreach (var member in teamMembers)
             {
-                member.showTeamMember();
+                skill += member.SkillLevel;
+            };
+            //for each trialRun number, do this loop
+            for(var i = 0; i < trialRuns; i++)
+            {
+                newBank.LadyLuck();
+                int breakInDifficulty = newBank.Difficulty + newBank.Luck;
+
+                Console.WriteLine($"Total team skill: {skill}");
+                Console.WriteLine($"Total bank difficulty: {breakInDifficulty}");
+
+
+                if (skill >= breakInDifficulty)
+                {
+                    Console.WriteLine("Success!");
+                    success++;
+                }
+                else
+                {
+                    Console.WriteLine("Your heist did not work");
+                    fail++;
+                }
             }
+            Console.WriteLine($"Successful runs: {success}. Failed Runs: {fail}.");
+            
         }
     }
 }
